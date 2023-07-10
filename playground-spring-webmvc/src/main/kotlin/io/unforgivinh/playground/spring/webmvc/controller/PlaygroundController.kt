bce52d1bdec3502f8c0e90fe576f97f295fd1ca2
vinh <unforgivinh@gmail.com>
2023-07-10 20:12:31 +0200

    build: include library 'commons-kotlin' for logging
    The 'commons-kotlin' library was added to the 'playground-spring-webmvc' pom.xml file. This addition is aimed to enable and improve logging capabilities.

--- playground-spring-webmvc/src/main/kotlin/io/unforgivinh/playground/spring/webmvc/controller/PlaygroundController.kt
+++ playground-spring-webmvc/src/main/kotlin/io/unforgivinh/playground/spring/webmvc/controller/PlaygroundController.kt
@@ -1,5 +1,6 @@
 package io.unforgivinh.playground.spring.webmvc.controller
 
+import com.crabshue.commons.kotlin.logging.getLogger
 import io.unforgivinh.playground.spring.webmvc.controllers.PlaygroundApi
 import io.unforgivinh.playground.spring.webmvc.dtos.PagePlayground
 import io.unforgivinh.playground.spring.webmvc.dtos.Playground
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class PlaygroundController : PlaygroundApi {
 
+    private val logger = getLogger()
+
     override fun getPlaygrounds(searchTerm: String?, pageable: Pageable?): ResponseEntity<PagePlayground> {
+
+        logger.info("Received request [GET /playgrounds] with parameters [searchTerm=$searchTerm] and [pageable=$pageable]")
 
         val ret = PagePlayground(
             listOf(
@@ -20,4 +25,5 @@ class PlaygroundController : PlaygroundApi {
         )
         return ResponseEntity.ok(ret)
     }
+
 }
