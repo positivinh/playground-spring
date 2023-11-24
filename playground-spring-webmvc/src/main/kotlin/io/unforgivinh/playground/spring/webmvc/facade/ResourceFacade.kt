f5a6d35a080965cfc17856c253628234089ee059
vinh <unforgivinh@gmail.com>
2023-11-24 18:24:53 +0100

    feat(openapi generation): use delegate pattern

--- playground-spring-webmvc/src/main/kotlin/io/unforgivinh/playground/spring/webmvc/facade/ResourceFacade.kt
+++ playground-spring-webmvc/src/main/kotlin/io/unforgivinh/playground/spring/webmvc/facade/ResourceFacade.kt
@@ -1,5 +1,6 @@
 package io.unforgivinh.playground.spring.webmvc.facade
 
+import io.unforgivinh.playground.spring.webmvc.controllers.ResourceApiDelegate
 import io.unforgivinh.playground.spring.webmvc.dtos.PageResource
 import io.unforgivinh.playground.spring.webmvc.dtos.Resource
 import io.unforgivinh.playground.spring.webmvc.mapper.ResourceMapper
@@ -9,18 +10,18 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 
 @Component
-class ResourceFacade(private val resourceService: ResourceService) {
+class ResourceFacade(private val resourceService: ResourceService) : ResourceApiDelegate {
 
     private val resourceMapper = Mappers.getMapper(ResourceMapper::class.java)
 
-    fun getPlaygrounds(searchTerm: String?, pageable: Pageable? = Pageable.unpaged()): PageResource {
-
-        val ret = resourceService.listResources(searchTerm, pageable)
-        return resourceMapper.mapPageable(ret)
-    }
-
-    fun createPlayground(resource: Resource): Resource {
+    override fun createResource(resource: Resource): Resource {
 
         return resourceService.createResource(resource)
+    }
+
+    override fun getResources(searchTerm: String?, pageable: Pageable?): PageResource {
+
+        val ret = resourceService.listResources(searchTerm, pageable ?: Pageable.unpaged())
+        return resourceMapper.mapPageable(ret)
     }
 }
