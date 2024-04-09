dfd520ce3339b44a8b4fd0682c4e9446ced32311
vinh <unforgivinh@gmail.com>
2024-04-09 23:36:50 +0200

    build: new module _playground-spring-mybatis_

---
+++ playground-spring-mybatis/src/main/kotlin/io/unforgivinh/playground/spring/mybatis/PlaygroundSpringMyBatisApplication.kt
@@ -0,0 +1,13 @@
+package io.unforgivinh.playground.spring.mybatis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaygroundSpringMyBatisApplication
+
+fun main(args: Array<String>) {
+
+    runApplication<PlaygroundSpringMyBatisApplication>(*args)
+}
+
---
+++ playground-spring-mybatis/src/test/kotlin/io/unforgivinh/playground/spring/mybatis/PlaygroundSpringMyBatisApplicationIT.kt
@@ -0,0 +1,12 @@
+package io.unforgivinh.playground.spring.mybatis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundSpringMyBatisApplicationIT {
+
+    @Test
+    fun contextLoads() {
+    }
+}
