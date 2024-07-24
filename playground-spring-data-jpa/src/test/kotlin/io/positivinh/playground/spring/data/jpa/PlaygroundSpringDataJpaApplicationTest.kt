caae2a686a21b12fe1a9c60d963d99936fee011b
vinh <positivinh@gmail.com>
2024-07-25 00:17:31 +0200

    build(playground-spring-data-jpa): new module `playground-spring-data-jpa`
    - place docker-compose.yaml files at the right location

---
+++ playground-spring-data-jpa/src/test/kotlin/io/positivinh/playground/spring/data/jpa/PlaygroundSpringDataJpaApplicationTest.kt
@@ -0,0 +1,16 @@
+package io.positivinh.playground.spring.data.jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundSpringDataJpaApplicationTest {
+
+    @Test
+    fun contextLoads() {
+
+    }
+
+
+}
+
