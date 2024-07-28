8ae2d180c2bfda2d20f77280cbb1c0b83e047f80
vinh <positivinh@gmail.com>
2024-07-28 17:44:18 +0200

    test(spring web mvc): fix controller tests

--- playground-spring-webmvc/README.md
+++ playground-spring-webmvc/README.md
@@ -1,6 +1,16 @@
 # Playground Spring Web MVC
 
+# Technologies
 
 - [Spring Web MVC](README/spring-webmvc.md)
 - [Open API 3 code generator](README/oas3-generation.md)
 
+# Run maven build
+
+```shell
+mvn clean verify -f ../pom.xml -am -pl playground-spring-webmvc
+```
+
+# API client
+
+Test HTTP client requests are available [here](./playground-spring-webmvc.http)
--- playground-spring-webmvc/src/test/kotlin/io/positivinh/playground/spring/webmvc/controller/ResourceControllerTest.kt
+++ playground-spring-webmvc/src/test/kotlin/io/positivinh/playground/spring/webmvc/controller/ResourceControllerTest.kt
@@ -1,25 +1,61 @@
 package io.positivinh.playground.spring.webmvc.controller
 
+import com.crabshue.commons.json.serialization.JsonSerializer
+import io.positivinh.playground.spring.webmvc.dtos.PageResource
+import io.positivinh.playground.spring.webmvc.dtos.Resource
+import io.positivinh.playground.spring.webmvc.facade.ResourceFacade
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.anyOrNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 class ResourceControllerTest {
+
+    @MockBean
+    private lateinit var resourceFacade: ResourceFacade
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Test
-    fun getPlaygrounds() {
+    fun getResources() {
 
-        mockMvc.get("/playgrounds")
-            .andExpect { MockMvcResultMatchers.status().isOk }
-            .andExpect { MockMvcResultMatchers.jsonPath("$.content.length()").value(2) }
+        val resourcePage = PageResource(listOf(Resource().name("playground"), Resource().name("other")))
+
+        Mockito.doReturn(resourcePage).`when`(resourceFacade).getResources(Mockito.any(), Mockito.any())
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/resources")
+        )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.content.length()").value(2))
+
+    }
+
+    @Test
+    fun createResource() {
+
+        val resource = Resource().name("playground")
+
+        Mockito.doAnswer { invokation -> invokation.arguments[0] }.`when`(resourceFacade).createResource(anyOrNull())
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/resources")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonSerializer.of(resource).serialize<String>())
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect { MockMvcResultMatchers.jsonPath("$.name").value("playground") }
     }
 }
