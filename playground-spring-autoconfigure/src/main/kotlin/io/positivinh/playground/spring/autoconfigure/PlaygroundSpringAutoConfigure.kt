3de0fa994cebdfeb002cbdfd959dccfb8ad5d265
vinh <positivinh@gmail.com>
2024-12-23 15:11:12 +0100

    refactor: use annotation `@ConfigurationPropertiesScan` instead of `EnableConfigurationProperties`

--- playground-spring-autoconfigure/src/main/kotlin/io/positivinh/playground/spring/autoconfigure/PlaygroundSpringAutoConfigure.kt
+++ playground-spring-autoconfigure/src/main/kotlin/io/positivinh/playground/spring/autoconfigure/PlaygroundSpringAutoConfigure.kt
@@ -4,14 +4,14 @@ import io.positivinh.playground.spring.autoconfigure.domain.PlaygroundBean
 import io.positivinh.playground.spring.autoconfigure.properties.PlaygroundAutoConfigurationProperties
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 
 /**
  * See [Reference documentation](https://docs.spring.io/spring-boot/reference/features/developing-auto-configuration.html#features.developing-auto-configuration.custom-starter)
  */
 @AutoConfiguration
-@EnableConfigurationProperties(PlaygroundAutoConfigurationProperties::class)
+@ConfigurationPropertiesScan
 class PlaygroundSpringAutoConfigure {
 
     @Bean
